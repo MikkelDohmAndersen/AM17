81c94e95996a960d02ded3d86c0a7c40210c0945
Mikkel Dohm Andersen <mikkelandersen4@hotmail.com>
2017-10-13 12:12:26 +0200

    The script now supplies a true or false statement if the moment of inertia for a profile is higher than needed

--- CrossSectionCalculator.py
+++ CrossSectionCalculator.py
@@ -1,9 +1,32 @@
-﻿""" Returns dimensions for the smallest cross section with a capacity larger than needed
+﻿""" Returns member index of all moments of inertia larger than needed
 
     Args:
         I: Moment of inertia needed
-        W: Width of profile
-        H: Height of profile
+        Iy: Moment of inertia y-axiz
+        Iz: Moment of inertia z-axiz
+    Returns:
+        i: member index
+
+"""
+Y=[]
+Z=[]
+
+#print (Iy)
+#print (Y)
+
+# Muliplying the list values to match input for Iy
+for i in Iy:
+    Y.append(i*10**6)
+print (Y)
+
+y=[]
+for i in Y:
+    y.append(i>I)
+print(y)
+
+Index = []
+
+
 
 
 
