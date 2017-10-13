7edbcf2d82ac0ad5d42fd87b9cf9ed4f03508b28
Mikkel Dohm Andersen <mikkelandersen4@hotmail.com>
2017-10-13 14:33:57 +0200

    Trying to combine two boolean lists to one, where a member is true if one of the two original lists have a true boolean at that index

---
+++ CombineBoolean.py
@@ -0,0 +1,19 @@
+﻿""" Combines two equal length lists of boolean statements
+    to one, either dominated by true or false values
+"""
+
+a = []
+
+#print(x)
+for i in x:
+    if i == 'True':
+        a.append('True')
+    else:
+        for i in y:
+            if i == 'True':
+                a.append('True')
+            else:
+                a.append('False')
+
+
+print(a)
