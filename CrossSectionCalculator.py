7c4c29db766a75763f9fde351cf0ca7a3c31ee86
Mikkel Dohm Andersen <mikkelandersen4@hotmail.com>
2017-10-13 13:34:19 +0200

    Clean up of script, descriptions added, works for both y and z axis

--- CrossSectionCalculator.py
+++ CrossSectionCalculator.py
@@ -1,30 +1,33 @@
-﻿""" Returns member index of all moments of inertia larger than needed
+﻿""" Returns member boolean statement indicating moments of inertia larger than needed
 
     Args:
         I: Moment of inertia needed
         Iy: Moment of inertia y-axiz
         Iz: Moment of inertia z-axiz
     Returns:
-        i: member index
-
+        PatternY: Boolean statement for capacity > needed for y axis
+        PatternZ: Boolean statement for capacity > needed for z axis
 """
 Y=[]
 Z=[]
 
-#print (Iy)
-#print (Y)
-
 # Muliplying the list values to match input for Iy
 for i in Iy:
     Y.append(i*10**6)
-print (Y)
+for i in Iz:
+    Z.append(i*10**6)
+#print (y)
 
-y=[]
+#Empty list for the new values
+PatternY=[]
+PatternZ=[]
+#Creating boolean statement for capacity > needed
 for i in Y:
-    y.append(i>I)
-print(y)
-
-Index = []
+    PatternY.append(i>I)
+print(PatternY)
+for i in Z:
+    PatternZ.append(i>I)
+print(PatternZ)
 
 
 
