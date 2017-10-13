10928997bb67e0ca6374e0a3ee16bbd6bdd4cb64
Mikkel Dohm Andersen <mikkelandersen4@hotmail.com>
2017-10-13 10:01:05 +0200

    First lines of code in calculation python script

---
+++ CrossSectionCalculator.py
@@ -0,0 +1,9 @@
+﻿""" Returns dimensions for the smallest cross section with a capacity larger than needed
+
+    Args:
+        I: Moment of inertia needed
+        W: Width of profile
+        H: Height of profile
+
+
+
