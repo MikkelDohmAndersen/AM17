e68abed477e0f1117be3c28824ef7d09d47627a0
Mikkel Dohm Andersen <mikkelandersen4@hotmail.com>
2017-11-03 16:26:17 +0100

    Code added for case 8

--- StrengthReductionMethod.py
+++ StrengthReductionMethod.py
@@ -37,7 +37,7 @@ elif z==4:
         dr.append(S[i]/A[i])
         Iysq.append(1/12*W[i]*(H[i]-2*dr[i])**3+1/12*W[i]*(2*dr[i])**3+W[i]*2*dr[i]*(H[i]/2)**2)
         Icr.append(2*Ir+Acr*((H[i]/2)+dr[i]-0.223*r)**2)
-        Itot.append(Iysq[i]-Icr[i])
+        Iytot.append(Iysq[i]-Icr[i])
         Pr.append(2*(H[i]-2*r)+(W[i]-2*r)+(W[i]-4*r)+(2*(m.pi*r*2)/4))
         Ar.append(W[i]*H[i]-2*Acr)
 #2*Width + Height
@@ -46,14 +46,10 @@ elif z==5:
 #2*Width + 2*Height
 elif z==8: 
     for i in range(len(W)):
-        A.append(W[i]*H[i]-2*Acr)
-        S.append(Acr*(H[i]/2-sr))
-        dr.append(S[i]/A[i])
-        Iysq.append(1/12*W[i]*(H[i]-2*dr[i])**3+1/12*W[i]*(2*dr[i])**3+W[i]*2*dr[i]*(H[i]/2)**2)
-        Icr.append(2*Ir+Acr*((H[i]/2)+dr[i]-0.223*r)**2)
-        Itot.append(Iysq[i]-Icr[i])
-        Pr.append(2*(H[i]-2*r)+(W[i]-2*r)+(W[i]-4*r)+(2*(m.pi*r*2)/4))
-        Ar.append(W[i]*H[i]-2*Acr)
+        Iytot.append(1/12*W[i]*H[i]**3-r**2*(4-m.pi)*(H[i]/2-sr)**2-4*Ir)
+        Iztot.append(1/12*H[i]*W[i]**3-r**2*(4-m.pi)*(W[i]/2-sr)**2-4*Ir)
+        Pr.append(2*(H[i]-2*r)+2*(W[i]-2*r)+m.pi*r*2)
+        Ar.append(W[i]*H[i]-4*Acr)
 else:
     print('You are doing something wrong')
 Iy=Iytot
