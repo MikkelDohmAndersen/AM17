0e46424b63cdf444535d240d25ed69d23b745918
Mikkel Dohm Andersen <mikkelandersen4@hotmail.com>
2017-11-13 12:02:17 +0100

    Calculation of a column exposed to a standard fire using reduced cross section method. Start on converting grasshopper spaghetti to pure python code

---
+++ Column-StdFire-RCS.py
@@ -0,0 +1,25 @@
+﻿"""
+Column - Standard fire with reduced cross section method
+
+    Args:
+        F: Vertical load for the beam to support (Calculated as a centrally placed load, also in the case when the column's center of gravity moves)
+        Str: Strength class for the wood
+        Sup: Support conditions for the column
+        ToW: Type of Wood; Sawn, Planed or Glulam (Glued laminated timber)
+        WS: Wood Species
+        t: Time of exposure
+        SEF: Sides Exposed to Fire
+    Returns:
+        NRc,fire: Charateristic resistance of the column after fire
+        Width: Width of the cross section before fire
+        Height: Height of the cross section before fire
+        Geo: 3D model of the cross section
+"""
+
+# Calculation of crossection after fire
+
+# Calculation of new moment of inertia
+
+# Calculation of NRc,fire
+
+# 3D model for "baking"
